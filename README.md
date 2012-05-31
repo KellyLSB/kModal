765dc79b7b9bddac18f194f24d6a558dae6de1a8
Kelly Becker <kellylsbkr@gmail.com>
2012-05-31 11:00:14 -0600

    documentation, and some bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-=== kModal = Kustom Modal ===
+kModal Kustom Modal
+===================
 kModal originally came up because we needed a modal that did not copy the div contents requiring us to rebind links. We since have developed it further to support custom styles. It was very important to us that we did not interfere with your modal style desires.
 
-=== How to use kModal ===
-Coming soon.
+How to use kModal
+=================
+To get started with kModal just include these lines into the bottom of your html file (or inside a `$(document).ready()` event)
+
+	<script src="/path/to/scripts/kModal.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	$(document).kModal('init' [, options]);
+	</script>
+
+This will append all the needed kModal elements, and set their default styles. After you can use these functions to manipulate your modals.
+
+	$(_element-id_).kModal('show'); // Opens the modal
+	$(_element-id_).kModal('hide'); // Closes the modal
+	$(_element-id_).kModal('resize'); // Repositions the modal (if you resize the modal for any reason you need to call this)
+
+Setting Options
+===============
+When intializing kModal with `$(document).kModal('init' [, options]);` you can pass the following options as a JSON object.
+
+	{
+		'mask-color': '#000', // Color of the mask
+		'mask-opacity': 0.3, // Opacity of the mask
+		'mask-z-index': 10, // Z-Index of the mask
+		'close-esc':1, // Bind the ESC key to close the modal
+		'close-mask':1, // Bind clicking on the mask to close the modal
+		'close-button':0, // Bind any immediate child of the modal that has the 'close' class to close the modal
+		'bind-link':1, // Bind any link containing the attribute kmodal="1" to open the modal whose selector matches that of the href attribute
+		'fade-time':500, // How long does it take to fade in / out
+		'modal-z-index': 11 // Modal Z-Index level
+	}
--- kModal.js
+++ kModal.js
@@ -5,6 +5,9 @@
 	var methods = {
 		init : function(options) {
 			
+			/**
+			 * Set the default settings
+			 */
 			settings = $.extend( {
 				'mask-color': '#000',
 				'mask-opacity': 0.3,
@@ -14,9 +17,13 @@
 				'close-mask':1,
 				'close-button':0,
 				'bind-link':1,
-				'fade-time':500
+				'fade-time':500,
+				'modal-z-index': 11,
 			} , options);
 			
+			/**
+			 * Insert the modal mask and set it's css
+			 */			
 			$('<div id="kmodal_mask"></div>').appendTo('body');
 
 			$('#kmodal_mask').css({
@@ -30,6 +37,9 @@
 				'display': 'none'
 		    });
 			
+			/**
+			 * Do we want to bind to links
+			 */
 			if(settings['bind-link']) {
 				$('a[kmodal=1]').live('click', function(e) {
 					e.preventDefault();
@@ -39,6 +49,9 @@
 				});
 			}
 			
+			/**
+			 * If a close button exists on the modal then close when it is clicked
+			 */
 			if(settings['close-button']) {
 				$('.kmodal .close').click(function(e) {
 					e.preventDefault();
@@ -48,6 +61,9 @@
 				});
 			}
 
+			/**
+			 * If we want to close the modal when the mask is clicked
+			 */
 			if(settings['close-mask']) {
 				$('#kmodal_mask').click(function(e) {
 					e.preventDefault();
@@ -57,6 +73,9 @@
 				});
 			}
 
+			/**
+			 * If we want the ESC key to close the modal bind it
+			 */
 			if(settings['close-esc']) {
 				$(document).keyup(function(e) {
 					e.preventDefault();
@@ -71,33 +90,76 @@
 		},
 		show : function(callback) {
 			
-			var maskHeight = $(document).height();
-			var maskWidth = $(window).width();
-
-			$('#kmodal_mask').css({
-				'width': maskWidth,
-				'height': maskHeight,
-			});
-			
+			/**
+			 * Resize the modal and position it
+			 */
 			$(this).kModal('resize');
 			
+			/**
+			 * Add the kmodal class to the modal
+			 */
 			$(this).addClass('kmodal');
 		
-			$('#kmodal_mask').fadeTo(settings['fade-time'], 0.66);
+			/**
+			 * Fade in the modal and the mask
+			 */
+			$('#kmodal_mask').fadeTo(settings['fade-time'], settings['mask-opacity']);
 			$(this).fadeIn(settings['fade-time']);			
 		},
 		hide : function(callback) {
+
+			/**
+			 * Fade out the modal and the mask
+			 */
 			$('#kmodal_mask').fadeOut(settings['fade-time']);
 			$(this).fadeOut(settings['fade-time']);
 		},
 		resize : function(callback) {
-			var winH = $(window).height();
+
+			/**
+			 * Get the document height for the mask
+			 */
+			var maskHeight = $(document).height();
+
+			/**
+			 * Get window size
+			 */
 			var winW = $(window).width();
-			
-			$(this).css('position', 'absolute');
-			$(this).animate({'top': winH / 2 - $(this).height() / 2}, settings['fade-time']);
-			$(this).animate({'left': winW / 2 - $(this).width() / 2}, settings['fade-time']);
-			$(this).css('z-index', 11);
+			var winH = $(window).height();
+
+			/**
+			 * Get modal size
+			 */
+			var modW = $(this).width();
+			var modH = $(this).height();
+
+			/**
+			 * Calculate position the modal needs to be in
+			 */
+			var resize = {
+				"top": (winH / 2) - (modH / 2),
+				"left": (winW / 2) - (modW / 2)
+			};
+
+			/**
+			 * Set the height and width of the mask
+			 */
+			$('#kmodal_mask').css({
+				'width': winW,
+				'height': maskHeight,
+			});
+
+			/**
+			 * Set the new values to the modal
+			 */
+			$(this).css('position', 'fixed');
+			$(this).animate(resize, settings['fade-time']);
+			$(this).css('z-index', settings['modal-z-index']);
+
+			/**
+			 * Return the position values
+			 */
+			return resize;
 		}
 	}
 	
